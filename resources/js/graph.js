46064f663481156c88dd34d4415e2a2f3f772f7e
Andrew Ripka <ripka@ripka-macbookpro.roam.corp.google.com>
2018-11-01 17:23:56 -0500

    update Chart.js to 2018 relevant versions

--- resources/js/graph.js
+++ resources/js/graph.js
@@ -2,99 +2,88 @@ $(document).ready( function(){
 
   $.ajax({method: "GET", url: "http://hwtcapi.rippmn.com/totalsByTime", async: false, success: function(graphData){
 
-  var myLineChart;
+    var myLineChart;
 
-  var treaters = {
-  labels : graphData['labels'],
-  datasets : [
-    {
-      label : "2007",
-      strokeColor : "#CC3399",
-      data : graphData['2007']
-    },
+    Chart.defaults.global.elements.point.radius=0;
 
-    {
-      label : "2008",
-      strokeColor : "#FFFF00",
-      data : graphData['2008']
-    },
+    //todo, build datasets betters
+    var treaters = {
+      "type":"line",
+      "data":{"labels":graphData['labels'],
+      "datasets":[
+          {
+            "label":"2007",
+            "data":graphData['2007'],
+            "fill":false,
+            "borderColor":"#CC3399"
+          },
+          {
+            "label":"2008",
+            "data":graphData['2008'],
+            "fill":false,
+            "borderColor":"#FFFF00"
+          },
+          {
+            "label":"2009",
+            "data":graphData['2009'],
+            "fill":false,
+            "borderColor":"#6600FF"
+          },
+          {
+            "label":"2010",
+            "data":graphData['2010'],
+            "fill":false,
+            "borderColor":"#ACC26D"
+          },
+          {
+            "label":"2011",
+            "data":graphData['2011'],
+            "fill":false,
+            "borderColor":"#009900"
+          },{
+            "label":"2012",
+            "data":graphData['2012'],
+            "fill":false,
+            "borderColor":"#FF00FF"
+          },{
+            "label":"2013",
+            "data":graphData['2013'],
+            "fill":false,
+            "borderColor":"#663300"
+          },{
+            "label":"2014",
+            "data":graphData['2014'],
+            "fill":false,
+            "borderColor":"#FF9900"
+          },{
+            "label":"2015",
+            "data":graphData['2015'],
+            "fill":false,
+            "borderColor":"#4B0082"
+          },{
+            "label":"2016",
+            "data":graphData['2016'],
+            "fill":false,
+            "borderColor":"#FF0000"
+          },{
+            "label":"2017",
+            "data":graphData['2017'],
+            "fill":false,
+            "borderColor":"#7FF50F"
+          },
+          {
+            "label":"2018",
+            "data":graphData['2018'],
+            "fill":false,
+            "borderColor":"#CA6F1E"
+          }
+        ]},
+      "options": {},
 
-    {
-      label : "2009",
-      strokeColor : "#6600FF",
-      fillColor : "#6600FF",
-      data : graphData['2009']
-    },
+      };
 
-    {
-      label : "2010",
-      strokeColor : "#ACC26D",
-      data : graphData['2010']
-    },
-
-    {
-      label : "2011",
-      strokeColor : "#009900",
-      data : graphData['2011']
-    },
-
-    {
-      label : "2012",
-      strokeColor : "#FF00FF",
-      data : graphData['2012']
-    },
-
-    {
-      label : "2013",
-      strokeColor : "#663300",
-      data : graphData['2013']
-    },
-
-    {
-      label : "2014",
-      strokeColor : "#FF9900",
-      data : graphData['2014']
-    },
-
-    {
-      label : "2015",
-      strokeColor : "#4B0082",
-      data : graphData['2015']
-    },
-
-    {
-      label : "2016",
-      strokeColor : "#FF0000",
-      fillColor : "#FF0000",
-      data : graphData['2016']
-    },
-
-    {
-      label : "2017",
-      strokeColor : "#7FF50F",
-      fillColor : "#7FF50F",
-      data : graphData['2017']
-    },
-
-    {
-      label : "2018",
-      strokeColor : "#CA6F1E",
-      fillColor : "#CA6F1E",
-      data : graphData['2018']
-    }
-
-  ]
-  };
+      var treatersChart = document.getElementById('MyChart').getContext('2d');
+      new Chart(treatersChart,treaters);
 
 
-  var treatersChart = document.getElementById('MyChart').getContext('2d');
-    myLineChart = new Chart(treatersChart).Line(treaters, {responsive: true, pointDot : false, bezierCurve : false, datasetFill : false, scaleShowLabels:true,
-      labelsFilter: function (value, index) {
-            return (index + 1) % 5 !== 0;
-        }
-    });
-    legend(document.getElementById("placeholder"), treaters);
-
-    }});
-
-});
+  });
